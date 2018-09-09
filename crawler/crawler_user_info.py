8bbbc1cf40810a2603fa2b5ff5e8a2dd84f4384e
goalkeeperfyc <664947387@qq.com>
2018-09-09 21:54:40 +0800

    two tasks on the way

---
+++ crawler/crawler_user_info.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  9 21:50:59 2018
+
+@author: fangyucheng
+"""
+
---
+++ task/get_data_everyday.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  9 21:53:22 2018
+
+@author: fangyucheng
+"""
+
